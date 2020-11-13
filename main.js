2513f68569782133bd355cab40024784846d8cf7
Kseniia <kbakhareva9@mail.ru>
2020-11-13 19:42:52 +0300

    Реализация infinite_scroll

---
+++ main.js
@@ -0,0 +1,16 @@
+function populate() {
+  while (true) {
+    let windowRelativeBottom = document.documentElement.getBoundingClientRect()
+      .bottom;
+    if (windowRelativeBottom > document.documentElement.clientHeight + 100)
+      break;
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `<p id="test_scroll"> Новый блок </p>`
+    );
+  }
+}
+
+window.addEventListener("scroll", populate);
+
+populate();
